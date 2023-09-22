0f77278e78e8c4c7aa3c32dd5f03182e125461ac
hun2 <hun2up@gmail.com>
2023-09-22 17:29:01 +0900

    course

--- pages/1_현황요약.py
+++ pages/1_현황요약.py
@@ -62,9 +62,11 @@ if authentication_status:
     df_sums = pd.DataFrame({'합계':df_sums}).transpose().drop(columns='소속부문')
     df_sums_apl = df_sums.drop(columns=['수료인원','수료누계','수료율','IMO신청인원','IMO신청누계','IMO신청률']).rename(columns={'신청인원':'목표인원','신청누계':'신청인원'})
     df_sums_apl.index = ['신청']
+    df_sums_apl = df_sums_apl.reset_index()
     df_sums_apl = df_sums_apl.rename(columns={'index':'과정명'})
     df_sums_atd = df_sums.drop(columns=['신청인원','신청누계','수료율','IMO신청인원','IMO신청누계','IMO신청률']).rename(columns={'수료인원':'목표인원','수료누계':'신청인원'})
     df_sums_atd.index = ['수료']
+    df_sums_atd = df_sums_atd.reset_index()
     df_sums_atd = df_sums_atd.rename(columns={'index':'과정명'})
     df_sums = pd.concat([df_sums_atd, df_sums_apl], axis=0)
     st.dataframe(df_sums)
