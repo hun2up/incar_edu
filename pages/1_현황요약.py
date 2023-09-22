8fd2c7c127a8abf25d6260ca9bf1b014950c905f
hun2 <hun2up@gmail.com>
2023-09-22 16:03:47 +0900

    course

--- pages/1_현황요약.py
+++ pages/1_현황요약.py
@@ -59,7 +59,7 @@ if authentication_status:
     # 신청수료인원
     df_stats = fn_status(df_all, '소속부문')
     df_sums = df_stats.sum(axis=0).transpose()
-    df_sums.name = '합계'
+    # df_sums.name = '합계'
     # df_sums = df_sums.transpose()
     st.dataframe(df_sums)
 
