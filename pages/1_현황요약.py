355c69f0b68b64ae408c271caf7e46420c77774e
hun2 <hun2up@gmail.com>
2023-10-04 13:26:49 +0900

    rankfa_apply

--- pages/1_현황요약.py
+++ pages/1_현황요약.py
@@ -125,11 +125,7 @@ if authentication_status:
     # ----------------------------------------------------  랭킹  -----------------------------------------------------------
     container_st_style = """
                         <style>
-                        .st-xyz .stContainer {
-                            border: 1px solid #000000;
-                            padding: 10px;
-                            margin: 10px;
-                            }
+                        .stContainer {border: 1px solid #000000; padding: 10px; margin: 10px;}
                         </style>
                         """
     st.markdown(container_st_style, unsafe_allow_html=True)
