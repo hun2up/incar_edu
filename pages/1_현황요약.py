560623b13886b10da896ecea1d38acd326e08e45
hun2 <hun2up@gmail.com>
2023-10-04 14:56:21 +0900

    rankfa_apply

--- pages/1_현황요약.py
+++ pages/1_현황요약.py
@@ -136,8 +136,7 @@ if authentication_status:
     r3_c1, r3_c2 = st.columns(2)
     rankfa_apply1, rankfa_apply2, rankfa_apply3, rankfa_apply4, rankfa_apply5 = r3_c1.columns(5)
     with rankfa_apply1.container():
-        st.write("컨테이너 테스트")
-        # st.metric(df_rank_fa.iat[0,1] + ' ' + df_rank_fa.iat[0,2], df_rank_fa.iat[0,3])
+        st.metric(df_rank_fa.iat[0,1] + ' ' + df_rank_fa.iat[0,2], df_rank_fa.iat[0,3])
     ########################################################################################################################
     ###########################################     stremalit 워터마크 숨기기     ##############################################
     ########################################################################################################################
