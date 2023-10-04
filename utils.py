8d8bb951906865f8441d1ea88fd08a596d37a021
hun2 <hun2up@gmail.com>
2023-10-04 16:17:53 +0900

    rankfa_apply

--- utils.py
+++ utils.py
@@ -370,6 +370,38 @@ def fig_piechart(label, value):
     fig_pchart.update_traces(hoverinfo='label+percent', textinfo='label+value', textfont_size=20)
     return fig_pchart
 
+def style_metric_cards(
+    background_color: str = "rgb(55,126,184)",
+    border_size_px: int = 1,
+    border_color: str = "#CCC",
+    border_radius_px: int = 5,
+    border_left_color: str = "rgb(179,205,227)",
+    box_shadow: bool = True,
+    text_color: str = "white",
+):
+
+    box_shadow_str = (
+        "box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important;"
+        if box_shadow
+        else "box-shadow: none !important;"
+    )
+    st.markdown(
+        f"""
+        <style>
+            div[data-testid="metric-container"] {{
+                background-color: {background_color};
+                border: {border_size_px}px solid {border_color};
+                padding: 5% 5% 5% 10%;
+                border-radius: {border_radius_px}px;
+                border-left: 0.5rem solid {border_left_color} !important;
+                {box_shadow_str}
+                color: {text_color}
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
 ########################################################################################################################
 ################################################     자료 전처리     ######################################################
 ########################################################################################################################
