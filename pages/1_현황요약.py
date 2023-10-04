659327f776c1f901c4236149f4a81fac562fee04
hun2 <hun2up@gmail.com>
2023-10-04 16:07:10 +0900

    rankfa_apply

--- pages/1_현황요약.py
+++ pages/1_현황요약.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import streamlit as st
 import streamlit_authenticator as stauth
-from streamlit_extras.stylable_container import stylable_container
+from streamlit_extras.metric_cards import style_metric_cards
 hashed_passwords = stauth.Hasher(['XXX']).generate()
 import yaml
 from yaml.loader import SafeLoader
@@ -129,19 +129,8 @@ if authentication_status:
     st.dataframe(df_rank_fa)
     r3_c1, r3_c2 = st.columns(2)
     rankfa_apply1, rankfa_apply2, rankfa_apply3, rankfa_apply4, rankfa_apply5 = r3_c1.columns(5)
-    # rankfa_apply1 = st.metric(df_rank_fa.iat[0,1] + ' ' + df_rank_fa.iat[0,2], df_rank_fa.iat[0,3])
-
-    with stylable_container(
-        key="container_with_border",
-        css_styles="""
-            {
-                border: 1px solid red;
-                border-radius: 0.5rem;
-                padding: calc(1em - 1px)
-            }
-            """,
-    ):
-        st.markdown(st.metric(df_rank_fa.iat[0,1] + ' ' + df_rank_fa.iat[0,2], df_rank_fa.iat[0,3]), unsafe_allow_html=True)
+    rankfa_apply1.metric(df_rank_fa.iat[0,1] + ' ' + df_rank_fa.iat[0,2], df_rank_fa.iat[0,3])
+    style_metric_cards()
 
     ########################################################################################################################
     ###########################################     stremalit 워터마크 숨기기     ##############################################
