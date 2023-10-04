eb70aa67b8380eb50980ed2d0a0d9647b922230b
hun2 <hun2up@gmail.com>
2023-10-04 14:54:15 +0900

    rankfa_apply

--- pages/1_현황요약.py
+++ pages/1_현황요약.py
@@ -12,6 +12,13 @@ with open('config.yaml') as file:
 from utils import fn_sidebar, fn_status, fn_trends, fig_piechart, generate_colors, generate_outsides, fig_hbarchart, fig_vbarchart, fn_rank_fa, fn_rank_partner, fn_rank_channel
 from utils import df_atd as df_all
 
+container_st_style = """
+                    <style>
+                    .stContainer {border: 5px solid red; padding: 10px; margin: 10px;}
+                    </style>
+                    """
+st.markdown(container_st_style, unsafe_allow_html=True)
+
 ########################################################################################################################
 ################################################     인증페이지 설정     ###################################################
 ########################################################################################################################
@@ -131,13 +138,6 @@ if authentication_status:
     with rankfa_apply1:
         st.write("컨테이너 테스트")
         # st.metric(df_rank_fa.iat[0,1] + ' ' + df_rank_fa.iat[0,2], df_rank_fa.iat[0,3])
-    
-    container_st_style = """
-                        <style>
-                        st-xyz .stContainer {border: 5px solid #ffffff; padding: 10px; margin: 10px;}
-                        </style>
-                        """
-    st.markdown(container_st_style, unsafe_allow_html=True)
     ########################################################################################################################
     ###########################################     stremalit 워터마크 숨기기     ##############################################
     ########################################################################################################################
