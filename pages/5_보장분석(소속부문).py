3a446697381a73461d9ec28ba5e05717a8d802af
hun2 <hun2up@gmail.com>
2023-10-20 16:06:12 +0900

    service

---
+++ pages/5_보장분석(소속부문).py
@@ -0,0 +1,50 @@
+########################################################################################################################
+##############################################     라이브러리 호출하기     ##################################################
+########################################################################################################################
+import streamlit as st
+import streamlit_authenticator as stauth
+hashed_passwords = stauth.Hasher(['XXX']).generate()
+import yaml
+from yaml.loader import SafeLoader
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+from utils import call_data
+from utils import month_dict
+from utils import ServiceData
+
+###########################################################################################################################
+################################################     인증페이지 설정     ###################################################
+###########################################################################################################################
+# ---------------------------------------------    페이지 레이아웃 설정    --------------------------------------------------
+st.set_page_config(page_title="실적관리 대시보드", layout='wide')
+
+# ----------------------------------------    Google Sheet 데이터베이스 호출    ---------------------------------------------
+# 9월 실적현황 SHEET 호출
+month = "sep"
+this_month = month_dict[month]
+df_month = call_data(month)
+
+# -------------------------------------------------  인증페이지 삽입  -------------------------------------------------------
+# 인증모듈 기본설정
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+# 로그인창 노출
+name, authentication_status, username = authenticator.login('Login', 'main')
+# 사이드바에 로그아웃 버튼 추가
+authenticator.logout('Logout', 'sidebar')
+# 인증상태 검증
+if authentication_status == None:
+    st.warning('아이디와 패스워드를 입력해주세요')
+if authentication_status == False:
+    st.error('아이디와 패스워드를 확인해주세요')
+if authentication_status:
+    ########################################################################################################################
+    ##################################################     자료 제작     #####################################################
+    ########################################################################################################################
+    instance = ServiceData(df_month)
+    st.dataframe(instance.make_data_service())
