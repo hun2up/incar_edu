c585756c1fa76999b47291421f9d54db8e6450ed
hun2 <hun2up@gmail.com>
2023-10-20 12:56:35 +0900

    class

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ import yaml
 from yaml.loader import SafeLoader
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
-from utils import fn_status, fn_trends, generate_colors, generate_outsides, generate_orders, fig_hbarchart, fig_vbarchart, fig_linechart
-from utils import df_apl
+# from utils import fn_status, fn_trends, fig_vbarchart
+# from utils import df_apl
+from utils import Chart
 
 ########################################################################################################################
 ################################################     인증페이지 설정     ###################################################
 ########################################################################################################################
+instance = Chart()
+df_apply = instance.call_data_apply("month")
+
+# -------------------------------------------------  인증페이지 삽입  -------------------------------------------------------
+# 인증모듈 기본설정
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
@@ -24,9 +30,11 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
-
+# 로그인창 노출
 name, authentication_status, username = authenticator.login('Login', 'main')
-
+# 사이드바에 로그아웃 버튼 추가
+authenticator.logout('Logout', 'sidebar')
+# 인증상태 검증
 if authentication_status == None:
     st.warning('아이디와 패스워드를 입력해주세요')
 if authentication_status == False:
@@ -36,18 +44,23 @@ if authentication_status:
     ##################################################     자료 제작     #####################################################
     #########################################################################################################################
     # -----------------------------------------------  당일 교육신청 현황  ---------------------------------------------------
-    # df_apl: 컬럼삭제 (소속부문,목표인원)
-    df_apl_line = df_apl.groupby(['날짜','과정명'])['신청인원'].sum().reset_index(name='신청인원')
-    ##### df_apl_line = ['날짜','과정명','신청인원']
-    df_apl_bar = df_apl.drop(df_apl[df_apl.iloc[:,0] != df_apl.iloc[-1,0]].index)
-    month_today = pd.to_datetime(df_apl_bar.iloc[-1]['날짜'], format="%Y. %m. %d").month
-    df_apl_bar = df_apl_bar.groupby(['날짜','과정명','목표인원'])['신청인원'].sum().reset_index(name='신청인원')
-    ##### df_apl_bar = ['날짜','과정명','목표인원','신청인원']
+    # df_apply_line = ['날짜','과정명','신청인원']
+    df_apply_line = df_apply.groupby(['날짜','과정명'])['신청인원'].sum().reset_index(name='신청인원')
+    # df_apply_bar = ['날짜','과정명','목표인원','신청인원']
+    df_apply_bar = df_apply.drop(df_apply[df_apl.iloc[:,0] != df_apply.iloc[-1,0]].index)
+    month_today = pd.to_datetime(df_apply_bar.iloc[-1]['날짜'], format="%Y. %m. %d").month
+    df_apply_bar = df_apply_bar.groupby(['날짜','과정명','목표인원'])['신청인원'].sum().reset_index(name='신청인원')
 
-    # ------------------------------------------  차트 제작에 필요한 리스트 제작  ---------------------------------------------
-    apl_colors = generate_colors(df_apl_bar.shape[0])
-    apl_outsides = generate_outsides(df_apl_bar.shape[0])
+    bar_today, line_today = st.columns(2)
+    line_today.plotly_chart(instance.make_linechart(
+        df=instance.make_set_trend(df=df_apply_line, columns='소속부문'),
+        category='과정명',
+        xaxis='날짜',
+        yaxis='신청인원',
+        title=f'{month_today}월 신청인원 추이'), use_container_width=True)
 
+
+    '''
     # -------------------------------------------------  barchart 제작  ------------------------------------------------------    
     barlist_apl = [df_apl_bar, apl_colors, apl_outsides, f'{month_today}월 신청인원 현황']
     bc_apl = fig_vbarchart(barlist_apl)
@@ -80,3 +93,4 @@ if authentication_status:
                     </style>
                     """
     st.markdown(hide_st_style, unsafe_allow_html=True)
+    '''
