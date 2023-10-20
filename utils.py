3c210d63aeb217beb6165def35ff6bd03f6fc131
hun2 <hun2up@gmail.com>
2023-10-20 19:51:46 +0900

    service

--- utils.py
+++ utils.py
@@ -480,7 +480,7 @@ class Register:
         df_enter['입사일자(사원)'] = df_enter['입사일자(사원)'].str.replace('/','').astype(int) # 입사일자의 형식을 8자리 숫자로 변환
         df_enter = df_enter[df_enter['입사일자(사원)'] >= 20230901].drop(columns=['입사일자(사원)']) # 특정일자 이후에 입사한 인원을 추출
         df_fa = df_fa[df_fa['사원번호'].isin(df_enter['사원번호'])] # 현재 재적인원에서 특정일자 이후에 입사한 인원 삭제
-        st.write(df_fa)
+        st.write(df_fa.shape[0])
         # enter = df_enter['사원번호'].tolist()
         # df_fa = df_fa[df_fa['사원번호'].isin(enter)] 
         # 퇴사인원관리 시트 호출
@@ -490,6 +490,6 @@ class Register:
         df_fa = pd.merge(df_fa, df_quit, on=['사원번호','영업가족CD'])
         # 입사 및 퇴사 시점에 맞게 재적인원 정리
         # df_fa = df_fa.merge(df_enter, on='사원번호', how='left', indicator=True).query('_merge == "left_only"').drop('_merge', axis=1)
-        st.write(df_fa)
+        st.write(df_fa.shape[0])
 
 
