1826416aee86b754ffa4bb9864bd9a6a15417915
hun2 <hun2up@gmail.com>
2023-10-20 16:47:56 +0900

    service

--- utils.py
+++ utils.py
@@ -444,5 +444,5 @@ class ServiceData:
         return df_result
     
     def make_service_branch(self):
-        df_result = self.make_service_data().groupby(['성명','사원번호'])['사원번호'].count()
+        df_result = self.make_service_data().groupby(['사원번호'])['사원번호'].count().reset_index(name='접속수')
         return df_result
