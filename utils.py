885cec31f10d269377d13e85bbed127534507db1
hun2 <hun2up@gmail.com>
2023-10-24 16:49:18 +0900

    oct

--- utils.py
+++ utils.py
@@ -242,7 +242,7 @@ class MakeSet(CallData):
             return int(month_name[:-1])
 
         # Sort the month names using the custom sorting key
-        sorted_month = {'월' : sorted(month_names, key=custom_sort_key)}
+        sorted_month = {'월' : sorted(month_names, key=int(month_names[:-1]))}
         df_apply = pd.merge(pd.DataFrame(sorted_month), df_apply, on=['월'])
         # 다 합쳐서 반환
         return df_apply
