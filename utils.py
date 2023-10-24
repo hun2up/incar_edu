0cc8e288a208256537275bbc45c26ca140d732fa
hun2 <hun2up@gmail.com>
2023-10-24 17:08:57 +0900

    oct

--- utils.py
+++ utils.py
@@ -402,14 +402,14 @@ class Chart(MakeSet):
     def make_cards_a(self, df, select, title):
         st.markdown('---')
         st.markdown(title)
-        index_card = [[['신청누계','수료율']], [['수료누계','수료율']], [['수료율','수료누계']], [['수료율','수료누계']]]
+        index_card = [['신청누계','수료율'], ['수료누계','수료율'], ['수료율','수료누계'], ['수료율','수료누계']]
         index_ascending = [False, False, False, True]
         index_columns = [3,5,6,6]
         index_units = ['회','회','%','%']
         # 랭킹 항목 4개씩 만들기
         for loop in range(4):
             st.write(select[loop])
-            df = df.sort_values(by=[index_card[loop]], ascending=[index_ascending[loop], False])
+            df = df.sort_values(by=[*index_card[loop]], ascending=[index_ascending[loop], False])
             # 카드 5개 씩 만들기
             sector = st.columns(5)
             for i in range(5):
