873c9f45cbd3b5a5bc5e4d0dc901de464e11bdb0
hun2 <hun2up@gmail.com>
2023-10-24 17:00:50 +0900

    oct

--- pages/1_교육관리(현황요약).py
+++ pages/1_교육관리(현황요약).py
@@ -97,6 +97,7 @@ if authentication_status:
     ##########################################################################################################################  
     style_metric_cards()
 
+    st.dataframe(instance.make_set_status(df_all,*['파트너','성명']))
     # [소속부문, 파트너, 성명, 신청인원, 신청누계, 수료인원, 수료누계, 수료율, IMO신청인원, IMO신청누계, IMO신청률]
     titles_fa = ['교육신청 TOP5 (FA)','교육수료 TOP5 (FA)','수료율 TOP5 (FA) (수료율 동률일 경우 수료누계 기준 순위정렬)','수료율 하위 TOP5 (FA) (수료율 동률일 경우 신청누계 기준 순위정렬)'] # 각 항목별 제목
     instance.make_cards_a(df=instance.make_set_status(df_all,*['파트너','성명']), select=titles_fa, title="##### 주요랭킹 (FA)")
