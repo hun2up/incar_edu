51496b881ae2bc0824edb2d648bfcb38b76a1ab9
hun2 <hun2up@gmail.com>
2023-10-25 09:24:16 +0900

    oct

--- utils.py
+++ utils.py
@@ -487,13 +487,12 @@ class ServiceData:
         ])
         columns_sum = {}
         for month_key, month_name in month_dict.items():
-            st.write(month_key)
-            st.write(month_name)
             with st.spinner(f"{month_name} 데이터를 불러오는 중입니다."):
                 for column_name in df_summary.columns:
                     df_month = self.make_service_data(month_key).drop(columns=['기준일자','소속부문','소속총괄','소속부서','파트너','사원번호','성명'])
                     columns_sum[column_name] = [df_month[column_name].sum()]
                 df_result = pd.DataFrame(columns_sum)
+                st.dataframe(df_result)
                 st.write(f"{month_name} 데이터 제작 완료")
                 try: df_summary = pd.concat(df_summary, df_result, axis=0)
                 except: break
