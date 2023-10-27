c9728f0e904a858fa390e2f766879cf6512e12c0
hun2 <hun2up@gmail.com>
2023-10-28 01:15:23 +0900

    change

--- utils.py
+++ utils.py
@@ -251,7 +251,7 @@ class MakeSet(CallData):
         for i in range(len(self.index)):
             df_two_total = df.groupby(['월',*columns])[self.index[i][0]].sum().reset_index(name=self.index[i][2]) # 수료현황 또는 IMO신청여부 : 전체 더하기 (수료누계 및 IMO신청누계)
             df_two_unique = pd.DataFrame(df[df[self.index[i][0]] != 0]).groupby(['월',*columns])['사원번호'].count().reset_index(name=self.index[i][1]) # 수료현황 또는 IMO신청여부 : 값이 1인 사원번호의 개수 (수료인원 및 IMO신청인원)
-            st.dataframe(df_two_unique)
+            st.dataframe(df_two_total)
         return df_apply
 
 
