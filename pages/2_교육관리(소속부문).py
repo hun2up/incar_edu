3e446d39a33cba9cbed903d394a2836958be6cf6
hun2 <hun2up@gmail.com>
2023-10-28 01:55:45 +0900

    change

--- pages/2_교육관리(소속부문).py
+++ pages/2_교육관리(소속부문).py
@@ -42,7 +42,7 @@ if authentication_status:
 
     # ------------------------------------------          인스턴스 생성          ---------------------------------------------
     instance = Chart()
-    df_channel = instance.call_data_change("attend", '소속부문')
+    df_channel = instance.call_data_change("attend")
     st.dataframe(instance.make_set_status(df_channel,'소속부문',*['소속부문']))
     #instance.make_set_change(df_channel,*['소속부문'])
 
