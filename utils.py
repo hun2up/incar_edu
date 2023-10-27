1ee9d926f977f1e96d81600b8b788028089132b8
hun2 <hun2up@gmail.com>
2023-10-27 18:24:28 +0900

    change

--- utils.py
+++ utils.py
@@ -204,25 +204,20 @@ class MakeSet(CallData):
     # ----------------------------          소속부문별 고유값 및 누계값 (상태값)          ---------------------------------
     def make_set_change(self, df, *columns): # *columns : '소속부문' 또는 '입사연차'
         # df : | 과정코드 | 과정분류 | 과정명 | 보험사 | 월 | 과정형태 | 수강료 | 지역 | 교육장소 | 정원 | 목표인원 | 소속부문 | 소속총괄 | 소속부서 | 파트너 | 사원번호 | 성명 | IMO신청여부 | 수료현황 | 입사연차
-        
+        st.dataframe(df)
         # 신청인원 및 신청누계 구하기
         df_apply_total = df.groupby([*columns,'사원번호']).size().reset_index(name='신청누계') # 신청누계 : df를 *columns로 묶고, 사원번호의 누적개수 구하기
         df_apply_unique = df_apply_total.groupby([*columns])['사원번호'].count().reset_index(name='신청인원') # 신청인원 : df를 *columns로 묶고, 사원번호의 고유개수 구하기
         df_apply = pd.merge(df_apply_unique, df_apply_total.groupby([*columns])['신청누계'].sum().reset_index(name='신청누계'), on=['소속부문']) # 신청인원과 신청누계 병합
         
+        # 수료인원, 수료누계, 수료율 및 IMO신청인원, IMO신청누계, IMO신청률
         for i in range(len(self.index)):
-            # df_attend = df.groupby(self.index[i][0]).get_group(1) # 수료현황 또는 IMO신청여부 : '1'로 묶기
             df_two_total = df.groupby([*columns])[self.index[i][0]].sum().reset_index(name=self.index[i][2]) # 수료현황 또는 IMO신청여부 : 전체 더하기 (수료누계 및 IMO신청누계)
-            df_two_unique = pd.DataFrame(df[df[self.index[i][0]] != 0]).groupby([*columns])['사원번호'].count().reset_index(name=self.index[i][1])
-            df_two = pd.merge(df_two_unique, df_two_total, on=[*columns]) # 수료인원이랑 수료누계 합치기
+            df_two_unique = pd.DataFrame(df[df[self.index[i][0]] != 0]).groupby([*columns])['사원번호'].count().reset_index(name=self.index[i][1]) # 수료현황 또는 IMO신청여부 : 값이 1인 사원번호의 개수 (수료인원 및 IMO신청인원)
+            df_two = pd.merge(df_two_unique, df_two_total, on=[*columns]) # 수료인원+수료누계 & IMO신청인원+IMO신청누계
             df_two[self.index[i][3]] = (df_two[self.index[i][2]]/df_apply['신청누계']*100).round(1) # 수료율 및 IMO신청률 구하기
-            df_apply = pd.merge(df_apply, df_two, on=[*columns])
-            # df_attend_total = df_attend_total.drop(columns=[self.index[i][2]])
-            # 수료율/IMO신청률 합치기
-            # df_attend = pd.merge(df_attend, df_attend_total, on=[*columns])
-            # df_apply = pd.merge(df_apply, df_attend, on=[*columns])
-        # df_units = df.groupby([*columns])['재적인원'].median().reset_index(name='재적인원')
-        st.dataframe(df_apply)
+            df_apply = pd.merge(df_apply, df_two, on=[*columns]) # 신청+수료+IMO
+        
 
     # ----------------------------          소속부문별 고유값 및 누계값 (상태값)          ---------------------------------
     def make_set_status(self, df, *columns):
