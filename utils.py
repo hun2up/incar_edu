1ec876330afd630eaf61b98c145a292467e25835
hun2 <hun2up@gmail.com>
2023-10-29 16:39:28 +0900

    change

--- utils.py
+++ utils.py
@@ -377,16 +377,16 @@ class EduPages(Charts):
             '누계인원':[df_summary['신청누계'].sum(), df_summary['수료누계'].sum()]
         })
     
-    '''
+    
     def make_summary_trend(self, df):
         # df_summary : | 월 | 소속부문/입사연차 | 신청인원 | 신청누계 | 수료인원 | 수료누계 | 수료율 | IMO신청인원 | IMO신청누계 | IMO신청률 | 재적인원 대비 신청인원 | 재적인원 대비 신청누계 | 재적인원 대비 수료인원 | 재적인원 대비 수료누계 | 재적인원 대비 IMO신청인원 | 재적인원 대비 IMO신청률'
-        df_summary = self.make_set_trend(df, *['월','소속부문'])
+        df_summary = self.make_set_trend(df,'소속부문', *['월','소속부문'])
         return pd.DataFrame({
             '월':[df_summary['월'].unique()],
             '신청누계':[df_summary.groupby(['월'])['신청누계'].sum()],
             '수료누계':[df_summary.groupby(['월'])['수료누계'].sum()]
         })
-    '''
+    
 
 #########################################################################################################################
 ################                        보장분석 클래스 정의 (데이터 정규화 - 호출)                        #################
