07d474587b5b3d0efb0ff72a3b84d38731c18e68
hun2 <hun2up@gmail.com>
2023-10-29 16:51:42 +0900

    change

--- utils.py
+++ utils.py
@@ -384,6 +384,7 @@ class EduPages(Charts):
         monthly_sums = df_summary.groupby(['월'])[['신청누계', '수료누계','재적인원']].sum()
         monthly_sums['재적인원 대비 신청누계'] = (monthly_sums['신청누계']/monthly_sums['재적인원']*100).round(1)
         monthly_sums['재적인원 대비 수료누계'] = (monthly_sums['수료누계']/monthly_sums['재적인원']*100).round(1)
+        '''
         # 월 데이터 오름차순 정렬
         month_names = monthly_sums['월']
         # Custom sorting key function to sort month names in the desired order
@@ -393,6 +394,7 @@ class EduPages(Charts):
             return int(month_name[:-1])
         sorted_month = {'월' : sorted(month_names, key=custom_sort_key)}
         monthly_sums = pd.merge(pd.DataFrame(sorted_month), monthly_sums, on=['월'])
+        '''
         return monthly_sums
         '''
         return pd.DataFrame({
