1c19939225d635d25cc2a23f06fcd724526f7b67
hun2 <hun2up@gmail.com>
2023-10-29 21:42:01 +0900

    add

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ if authentication_status:
     )
 
     # -----------------------------------------------  당일 교육신청 현황  ---------------------------------------------------
+    # 첫번째 행 (과정별 신청현황, 과정별 신청추이)
     bar_today, line_today = st.columns(2)
     bar_today.plotly_chart(instance.make_vbarchart(
         df=df_main.drop(df_main[df_main.iloc[:,0] != df_main.iloc[-1,0]].index).groupby(['신청일자','과정명','목표인원'])['신청인원'].sum().reset_index(name='신청인원'),
@@ -70,6 +71,7 @@ if authentication_status:
         yaxis='신청인원',
         title=f'{pd.to_datetime(df_main.iloc[-1,0], format="%Y. %m. %d").month}월 신청인원 추이'), use_container_width=True)
     
+    # 두번째 행 (소속부문별 신청현황, 입사연차별 신청현황)
     hbar_channel, hbar_career = st.columns(2)
     hbar_channel.plotly_chart(instance.make_hbarchart_single(
         df=instance.make_set_main(df_main,'소속부문'),
@@ -82,4 +84,5 @@ if authentication_status:
         axis_a='신청인원',
         title='입사연차별 신청인원 현황'), use_container_width=True)
 
+    # 세번째 행 (신청현황 리스트)
     st.dataframe(df_main.drop(df_main[df_main.iloc[:,0] != df_main.iloc[-1,0]].index)[['교육일자','과정명','소속부문','파트너','사원번호','성명']], use_container_width=True) # 마지막 신청일자 제외한 나머지 신청내역 삭제
