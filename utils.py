f648d244266f724e55b7814946f5f9abdfa27961
hun2 <hun2up@gmail.com>
2023-10-31 10:45:09 +0900

    service

--- utils.py
+++ utils.py
@@ -496,9 +496,9 @@ class ServiceData:
             df_sum = pd.DataFrame()
             df_part_sum = pd.DataFrame()
             for i in df_part.columns:
-                #if i in ['소속부문','소속총괄']: df_sum[i] = df_part[i]
-                #elif i in ['소속부서']: df_sum[i] = ''
-                df_sum[i] = df_part[i].sum() # 컬럼별 합계
+                if i in ['소속부문','소속총괄']: df_sum[i] = df_part[i]
+                elif i in ['소속부서']: df_sum[i] = ''
+                else: df_sum[i] = df_part[i].sum() # 컬럼별 합계
                 # df_sum['소속부문'] = df_part['소속부문']
                 # df_sum['소속총괄'] = df_part['소속총괄']
                 # df_sum['소속부서'] = ''
