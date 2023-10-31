bd6a4d3101234d0c3e5e3eeceed31c8aabc69224
hun2 <hun2up@gmail.com>
2023-10-31 12:18:08 +0900

    service

--- utils.py
+++ utils.py
@@ -496,19 +496,19 @@ class ServiceData:
             for part in df_channel['소속총괄'].unique():
                 df_part = df_channel[df_channel['소속총괄'].isin([part])] # 소속총괄별로 데이터 추출 (소속부서별 접속자수)
                 df_sum = pd.DataFrame() # 산하 소속부서의 월별 접속자수 합계
-                for i in df_part.columns:
-                    if i in ['소속부문','소속총괄']: df_sum[i] = df_part[i]
-                    elif i in ['소속부서']: df_sum[i] = ''
-                    else: df_sum[i] = df_part[i].sum() # 컬럼별 합계
+                for part_detail in df_part.columns:
+                    if part_detail in ['소속부문','소속총괄']: df_sum[part_detail] = df_part[part_detail]
+                    elif part_detail in ['소속부서']: df_sum[i] = ''
+                    else: df_sum[part_detail] = df_part[part_detail].sum() # 컬럼별 합계
                 df_sum = df_sum.iloc[[0]] # 소속부서 개수만큼 생성된 동일한 내용의 ROW를 하나만 남기고 삭제
                 df_part = pd.concat([df_part, df_sum], axis=0) # 소속부서별 접속자수 데이터프레임 하단에 소속총괄 합계 추가
                 df_total = pd.concat([df_total, df_part], axis=0) # 소속부문 산하 전체 소속총괄 데이터 병합
         # ---------------------------------------------------------------------------------------------------------------
             df_sum = pd.DataFrame() # 산하 소속총괄의 월별 접속자수 합계
-            for i in df_channel.columns:
-                if i in ['소속부문']: df_sum[i] = df_channel[i]
-                elif i in ['소속총괄','소속부서']: df_sum[i] = ''
-                else: df_sum[i] = df_channel[i].sum()
+            for channel_detail in df_channel.columns:
+                if channel_detail in ['소속부문']: df_sum[channel_detail] = df_channel[channel_detail]
+                elif channel_detail in ['소속총괄','소속부서']: df_sum[channel_detail] = ''
+                else: df_sum[channel_detail] = df_channel[channel_detail].sum()
             df_sum = df_sum.iloc[[0]] # 소속부서 개수만큼 생성된 동일한 내용의 ROW를 하나만 남기고 삭제
             df_total = pd.concat([df_total, df_sum], axis=0)
         return df_total
