d52a67ccb374c41b618b78c09861a1c1334e87ef
hun2 <hun2up@gmail.com>
2023-10-31 10:35:35 +0900

    service

--- utils.py
+++ utils.py
@@ -499,5 +499,7 @@ class ServiceData:
                 df_sum[i] = [df_part[i].sum()] # 컬럼별 합계
                 df_sum['소속부서'] = ''
                 df_part_sum = pd.concat([df_part, df_sum], axis=0)
+                if part in ['CA1총괄']: st.dataframe(df_part_sum)
+                else: pass
             df_branch_sum = pd.concat([df_branch, df_part_sum], axis=0)
         return df_branch_sum
