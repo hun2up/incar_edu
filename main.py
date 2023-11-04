d7f842c5e9feeabd5e7054f751737ace6a2bb28a
hun2 <hun2up@gmail.com>
2023-11-04 16:21:19 +0900

    effi

--- main.py
+++ main.py
@@ -104,11 +104,15 @@ if authentication_status:
 
     df_apply = df_main.drop(df_main[df_main.iloc[:,0] != df_main.iloc[-1,0]].index)[['교육일자','과정명','소속부문','파트너','사원번호','성명','입사연차']]
     df_target = instance.make_set_target()
-    st.dataframe(df_apply)
-    st.dataframe(df_target)
     df_apply_target = pd.DataFrame()
     apply_unique = df_target['사원번호'].unique()
     for apply in apply_unique:
         df_compare = df_apply[df_apply['사원번호'] == apply]
         df_apply_target = pd.concat([df_apply_target, df_compare], axis=0)
+
+    
+    st.dataframe(df_apply_target)
+
+    test = df_apply[df_apply['사원번호'] == df_target['사원버  ']]
+    st.dataframe(test)
     
