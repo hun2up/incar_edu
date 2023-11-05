1503a93dad0b94378f7ff5da095552ffe1041d18
hun2 <hun2up@gmail.com>
2023-11-05 16:32:13 +0900

    main

--- main.py
+++ main.py
@@ -96,9 +96,10 @@ if authentication_status:
     chart_all.dataframe(df_main.drop(df_main[df_main.iloc[:,0] != df_main.iloc[-1,0]].index)[['신청일자','교육일자','과정명','소속부문','파트너','사원번호','성명','입사연차']].reset_index(drop=True), use_container_width=True) # 마지막 신청일자 제외한 나머지 신청내역 삭제
 
     df_result = pd.DataFrame()
-    df_bar = instance.make_set_target(df=df_main, merge_type='left')[['과정명','사원번호','타겟명']]
-    df_result = df_bar.groupby(['과정명','타겟명'])['사원번호'].nunique().reset_index(name='신청인원')
-    st.dataframe(df_result)
+    df_all = instance.make_set_target(df=df_main, merge_type='left').groupby(['과정명'])['사원번호'].nunique().reset_index(name='신청인원')
+    df_target = instance.make_set_target(df=df_main, merge_type='left').groupby(['과정명','타겟명'])['사원번호'].nunique().reset_index(name='유입인원')
+    df_bar = pd.merge(df_all, df_target, on='과정명', how='left')
+    st.dataframe(df_bar)
     
 
     
