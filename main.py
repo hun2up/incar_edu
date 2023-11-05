60b7970e65c368c17568f577560de07fa25a7cef
hun2 <hun2up@gmail.com>
2023-11-05 16:15:26 +0900

    main

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if authentication_status:
     chart_new.dataframe(instance.make_set_new(df_main), use_container_width=True)
     chart_all.dataframe(df_main.drop(df_main[df_main.iloc[:,0] != df_main.iloc[-1,0]].index)[['신청일자','교육일자','과정명','소속부문','파트너','사원번호','성명','입사연차']].reset_index(drop=True), use_container_width=True) # 마지막 신청일자 제외한 나머지 신청내역 삭제
 
-    st.dataframe(instance.apply_by_target(df_main))
+    st.dataframe(instance.make_set_target(df=df_main, merge_type='left'))
     # 네번째 행 (신청현황 리스트)
     st.markdown('---')
     pie_apply, pie_target, bar_compare = st.columns(3)
